6e994610cc635dda24ad3389a3d42e9328a6c6e2
name <srpiroliro21@gmail.com>
2023-06-15 20:25:17 +0200

    rest contollers.

--- src/main/java/com/tecnocampus/bank/api/AccountRestController.java
+++ src/main/java/com/tecnocampus/bank/api/AccountRestController.java
@@ -1,5 +1,41 @@
 package com.tecnocampus.bank.api;
 
+import com.tecnocampus.bank.application.AccountController;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+// @ ... ("/customers/{customerId}")
 public class AccountRestController {
-    
+    private AccountController accountController;
+
+    public AccountRestController(AccountController accountController){
+        this.accountController=accountController;
+    }
+
+    @GetMapping("/customers/{customerId}/accounts")
+    public List<AccountDTO> getCustomerAccounts(@PathVariable String customerId) throws Exception {
+        return accountController.getAccountsByCustomerId(customerId);
+    }
+    @PostMapping("/customers/{customerId}/accounts")
+    public List<AccountDTO> createAccount(@PathVariable String customerId, @RequestBody AccountDTO accountDTO) throws Exception {
+        return accountController.createAccount(customerId, accountDTO);
+    }
+
+    @GetMapping("/customers/{customerId}/accounts/{accountId}")
+    public List<AccountDTO> getAccount(@PathVariable String customerId, @PathVariable String accountId) throws Exception {
+        return accountController.getAccount(accountId);
+    }
+
+    @DeleteMapping("/customers/{customerId}/accounts/{accountId}")
+    public void deleteAccount(@PathVariable String customerId, @PathVariable String accountId) throws Exception {
+        accountController.deleteAccount(accountId);
+    }
 }
--- src/main/java/com/tecnocampus/bank/api/CustomerRestController.java
+++ src/main/java/com/tecnocampus/bank/api/CustomerRestController.java
@@ -1,5 +1,50 @@
 package com.tecnocampus.bank.api;
 
+import com.tecnocampus.bank.application.CustomerController;
+import com.tecnocampus.bank.application.dto.CustomerDTO;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RestController
 public class CustomerRestController {
-    
+    private CustomerController customerController;
+
+    public CustomerRestController(CustomerController customerController){
+        this.customerController=customerController;
+    }
+
+
+
+    @GetMapping("/customers")
+    public CustomerDTO getAllCustomers() throws Exception {
+        return customerController.getAllCustomers();
+    }
+
+    @PostMapping("/customers")
+    public CustomerDTO createCustomer(@RequestBody CustomerDTO customerDTO) throws Exception {
+        return customerController.createCustomer(customerDTO);
+    }
+
+
+
+    @GetMapping("/customers/{id}")
+    public CustomerDTO getCustomer(@PathVariable String id) throws Exception {
+        return customerController.getCustomer(id);
+    }
+
+    @PutMapping("/customers/{id}")
+    public CustomerDTO updateCustomer(@PathVariable String id) throws Exception {
+        return customerController.updateCustomer(id);
+    }
+
+    @DeleteMapping("/customers/{id}")
+    public CustomerDTO deleteCustomer(@PathVariable String id) throws Exception {
+        return customerController.deleteCustomer(id);
+    }   
 }
